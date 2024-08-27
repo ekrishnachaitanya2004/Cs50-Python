314aaddd24f8036586b42168ed02d18eace9f107
Krishna Chaiatanya <ekrishnachaitanya2004@gmail.com>
2024-08-27 21:52:04 +0530

    Problem Set 1

---
+++ indoor/indoor.py
@@ -0,0 +1,6 @@
+# Ask Input from user
+user_input = input("")
+
+
+# In Python, there is a built-in method that can change a string that is in uppercase to lowercase. It also applies to strings that have letters both in uppercase and lowercase. The “.lower() “ method changes the strings to lowercase.
+print(user_input.lower())
